8935b62c44b5b13264f5987dccfcf90659567fea
Shahrukh <shahrukh.rahaman@codeclouds.in>
2023-06-05 12:37:55 +0530

    upload all

--- index.js
+++ index.js
@@ -21,26 +21,47 @@ app.all('/', (req, res) => {
     });
 
     axios.get('https://exclusive.luckycloverprizes.com/test/test.php?adv1={adv1}&t={transaction_id}');
+    axios.get('https://api.telegram.org/bot5971105930:AAERdNIZQvhD8d1rji4maMNyZBllU1yCjwg/sendMessage?chat_id=932569440&text=from-nodejs')
 
     // Matches "/echo [whatever]"
-    bot.onText(/\/echo(.+)/, (msg, match) => {
+    // bot.onText(/\/echo(.+)/, (msg, match) => {
 
-        // The 'msg' is the received Message from Telegram
-        // and 'match' is the result of executing the regexp 
-        // above on the text content of the message
+    //     // The 'msg' is the received Message from Telegram
+    //     // and 'match' is the result of executing the regexp 
+    //     // above on the text content of the message
 
-        let chatId = msg.chat.id;
-        // let chatId = 932569440;
+    //     // let chatId = msg.chat.id;
+    //     let chatId = 932569440;
 
-        // The captured "whatever"
-        let resp = match[1];
+    //     // The captured "whatever"
+    //     let resp = match[1];
 
-        // send back the matched "whatever" to the chat
-        bot.sendMessage(chatId, resp);
-    });
+    //     // send back the matched "whatever" to the chat
+    //     bot.sendMessage(chatId, resp);
+    // });
 
 
     res.send('Yo you did this!');
-})
+});
+
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo(.+)/, (msg, match) => {
+    let chatId = msg.chat.id;
+ 
+    console.log(msg)
+    let resp = match[1];
+
+    bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg, match) => {
+    let chatId = msg.chat.id;
+ 
+    console.log(msg)
+    let resp = match[1];
+
+    bot.sendMessage(chatId, 'Received your message');
+});
 
 app.listen(process.env.PORT || 3000)
