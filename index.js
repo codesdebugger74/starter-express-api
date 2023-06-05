058cff444875b9eb2ed51d2efeaa4e402ab30344
Shahrukh <shahrukh.rahaman@codeclouds.in>
2023-06-05 16:41:17 +0530

    1

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-// const axios = require('axios');
 var request = require('request');
 let TelegramBot = require('node-telegram-bot-api');
 
@@ -12,7 +11,7 @@ let bot = new TelegramBot(token, { polling: true });
 app.all('/', (req, res) => {
     console.log("Just got a request!");
 
-    console.log(req.body);
+    console.log(req.body.current_state);
 
     const check_name = req.body.check_name;
     const previous_state = req.body.previous_state;
@@ -28,7 +27,7 @@ app.all('/', (req, res) => {
         {
             img_src = '🟢';
         }
-        const tg_msg = `[${current_state} ${img_src}] ${check_name}`;
+        const tg_msg = `[${current_state} ${img_src}] ${check_name} @ShahrukhCC`;
         request(encodeURI(`https://api.telegram.org/bot5971105930:AAERdNIZQvhD8d1rji4maMNyZBllU1yCjwg/sendMessage?chat_id=-1001849542707&text=${tg_msg}&parse_mode=html`), function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 console.log(body) // Print the google web page.
@@ -36,33 +35,22 @@ app.all('/', (req, res) => {
         });
     // }
 
-    // let msgtest = `🟢`;
-    // console.log(`https://api.telegram.org/bot5971105930:AAERdNIZQvhD8d1rji4maMNyZBllU1yCjwg/sendMessage?chat_id=-1001849542707&text=${msgtest}&parse_mode=html`);
-
-    // request(encodeURI(`https://api.telegram.org/bot5971105930:AAERdNIZQvhD8d1rji4maMNyZBllU1yCjwg/sendMessage?chat_id=-1001849542707&text=${msgtest}&parse_mode=html`), function (error, response, body) {
-    //     if (!error && response.statusCode === 200) {
-    //         console.log(body) // Print the google web page.
-    //     }
-    // });
-
-
-
-    // axios.get('https://api.telegram.org/bot5971105930:AAERdNIZQvhD8d1rji4maMNyZBllU1yCjwg/sendMessage?chat_id=932569440&text=from-nodejs');
-
-
-
-    // fs.appendFile('./logs/test.txt', JSON.stringify(req.body), err => {
-    //     if (err) {
-    //         console.error(err);
-    //     }
-    //     // file written successfully
-    // });
-
-    // axios.get('https://exclusive.luckycloverprizes.com/test/test.php?adv1={adv1}&t={transaction_id}');
-
-
     res.send('Yo you did this!');
 });
+
+app.listen(process.env.PORT || 3000)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Matches "/echo [whatever]"
@@ -83,5 +71,3 @@ app.all('/', (req, res) => {
 
 //     bot.sendMessage(chatId, 'Received your message');
 // });
-
-app.listen(process.env.PORT || 3000)
