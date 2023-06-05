545c7c3801ff11fcd4beed92b3a94f45df280a6d
Shahrukh <shahrukh.rahaman@codeclouds.in>
2023-06-05 11:22:33 +0530

    upload add

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo you did this!')
+    console.log("Just got a request!");
+
+    console.log(req.body);
+
+    fs.appendFile('./logs/test.txt', JSON.stringify(req.body), err => {
+        if (err) {
+            console.error(err);
+        }
+        // file written successfully
+    });
+
+    res.send('Yo you did this!');
 })
 app.listen(process.env.PORT || 3000)
